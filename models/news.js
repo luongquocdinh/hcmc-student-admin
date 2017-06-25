c001c466ba3b282e585caddfa4bafd470c29fa45
quocdinhcs <luongquocdinh.95@gmail.com>
2017-06-25 21:13:55 +0700

    test

--- models/news.js
+++ models/news.js
@@ -3,19 +3,16 @@ var Schema = mongoose.Schema
 mongoose.Promise = require('bluebird');
 
 var news = new Schema({
+    source: {type: String },
     topic: {type: String},
     topic_ascii: {type: String},
-    is_enable: {type: Boolean},
-    news: [{
-        title: {type: String},
-        thumbnail: {type: String},
-        brief: {type: String},
-        content: {type: String},
-        is_accept: {type: Boolean},
-        concern: {type: String},
-        created_at: { type: Date, default: Date.now},
-        updated_at: { type: Date, default: Date.now}
-    }],
+
+    title: {type: String},
+    thumbnail: {type: String},
+    brief: {type: String},
+    content: {type: String},
+    is_accept: {type: Boolean},
+
     created_at: { type: Date, default: Date.now },
     updated_at: { type: Date, default: Date.now },
 },
--- models/topic.js
+++ models/topic.js
@@ -2,9 +2,9 @@ var mongoose = require('mongoose')
 var Schema = mongoose.Schema
 
 var topic = new Schema({
-    id_topic: Schema.Types.ObjectId,
-    name_source: String,
-    name_topic: String
+    topic_ascii: String,
+    source: String,
+    topic: String
 })
 
 var Topic = mongoose.model('topic', topic)
