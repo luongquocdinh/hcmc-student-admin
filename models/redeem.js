067274e9a8657859ad2f7a61c90e229cb08c71cb
dinhlq <dinhlq@9action.com>
2018-01-02 21:21:20 +0700

    fix bug

---
+++ models/redeem.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+mongoose.Promise = require('bluebird');
+
+var redeem = new Schema({
+    user_id: {type: String, index: true},
+    email: {type: String},
+    name: {type: String},
+    phone: {type: String},
+    reward_id: {type: String, index: true},
+    serial: {type: String},
+
+    created_date: {type: Date, default: Date.now()},
+    updated_date: {type: Date, default: Date.now()}
+})
+
+var Redeem = mongoose.model('redeem', redeem)
+
+module.exports = Redeem
